c0ca5ecd61bbff1a93ea8d2c460b8607d7490336
fdwillis <fdwillis7@gmail.com>
2015-09-21 16:28:27 -0500

    starting backend styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
   def home
+    unless !current_user
+        redirect_to fundraising_goals_path
+    end
     #Track For Admin
     # if !current_user  
     #   Keen.publish("Homepage Visits", { 
