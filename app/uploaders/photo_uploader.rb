5e48ce6389b02d4750db113486b1f04661fa0825
fdwillis <fdwillis7@gmail.com>
2015-10-22 11:37:32 -0500

    new size for images

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -9,24 +9,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
      %w(jpg jpeg gif png)
   end
 
- version :p150x150 do
-   process :resize_and_pad => [150,150]
- end
- # version :p200x200 do
- #   process :resize_and_pad => [200,200]
- # end
- version :p400x400 do
-   process :resize_and_pad => [400,400]
+ version :p303x498 do
+   process :resize_to_fit => [303,498]
  end
  # version :p540x160 do
  #   process :resize_to_fit => [540, 160]
- #   #process :resize_and_pad => [540,160]
+ #   #process :resize_to_fit => [540,160]
  # end
  # version :p530x150 do
- #   process :resize_and_pad => [530,150]
+ #   process :resize_to_fit => [530,150]
  # end
- version :p500x350 do
-   process :resize_and_pad => [500,350]
- end
-
 end
