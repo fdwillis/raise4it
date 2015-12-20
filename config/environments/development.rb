4062226a4712d9503c8bb74569babace974cfaaf
fdwillis <fdwillis7@gmail.com>
2015-12-20 15:33:52 -0600

    precompiled assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,5 +46,4 @@ Rails.application.configure do
   config.action_mailer.preview_path = "#{Rails.root}/lib/mailer_previews"
 
   config.middleware.use Rack::TwilioWebhookAuthentication, ENV["TWILIO_AUTH_TOKEN"] , '/voice'
-  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
 end
